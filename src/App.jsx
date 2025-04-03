22c960ab493697b2bb9724bbec65d22ac94833fe
Glitch (gossamer-outgoing-advantage) <none>
2025-04-03 08:51:55 +0000

    🚎🎸 Checkpoint ./src/App.jsx:77106476/7639 ./src/components/EventDetailsInput.jsx:77106476/2260

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import { MetaContext } from "./context/PixelContext";
-import Input from "./components/Input";
-import { fbEvents } from "./config/fbEvents";
 import { ConfigInput } from "./components/ConfigInput";
 import ReactPixel from "react-facebook-pixel";
-import { userInfoFieldsJson } from "./config/userInfoJson";
+import EventDetailsInput from "./components/EventDetailsInput";
+import UserDetailsInput from "./components/UserDetailsInput";
 
 function App() {
   const { state, updateState } = useContext(MetaContext);
@@ -36,9 +35,9 @@ function App() {
       ReactPixel.init(state.pixelId, userInfo);
       ReactPixel.track(eventType, dataParams);
       console.log(
-        `Sending tracking event: ${eventType}\nWith no user details\nWith data params: {${Object.entries(dataParams).map(
-          ([key, value]) => `${key}:${value}`
-        )}}`
+        `Sending tracking event: ${eventType}\nWith no user details\nWith data params: {${Object.entries(
+          dataParams
+        ).map(([key, value]) => `${key}:${value}`)}}`
       );
     }
     setDataParams({});
@@ -62,113 +61,15 @@ function App() {
     <div style={{ width: "100vw" }}>
       <ConfigInput state={state} handleUpdate={handleUpdate} />
       <hr />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-          marginBottom: "0.5rem",
-        }}
-      >
-        <span>User Info: (Optional):</span>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "row",
-            alignItems: "center",
-            columnGap: "0.25rem",
-          }}
-        >
-          {userInfoFieldsJson.map((field) => (
-            <Input
-              key={field.label}
-              onChange={handleUserInfo}
-              id={field.parameter}
-              label={field.label}
-              value={userInfo[field.parameter]}
-            />
-          ))}
-        </div>
-        <button onClick={() => setUserInfo({})}>Delete</button>
-      </div>
+      <UserDetailsInput onChange={handleUserInfo} userInfo={userInfo} />
       <hr />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyItems: "center",
-            alignItems: "center",
-            paddingBottom: "0.5rem",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              columnGap: "0.25rem",
-              paddingBottom: "0.25rem",
-            }}
-          >
-            <label htmlFor="eventType">Event Type:</label>
-            <select
-              id="eventType"
-              onChange={handleEventSelect}
-              value={eventType}
-            >
-              <option>Select event type</option>
-              {fbEvents.map((event) => (
-                <option key={event.name}>{event.name}</option>
-              ))}
-            </select>
-          </div>
-          {eventType && (
-            <span style={{ fontStyle: "italic", fontSize: "0.75rem" }}>
-              {fbEvents.find((event) => event.name === eventType).description}
-            </span>
-          )}
-        </div>
-        {eventType && (
-          <div style={{ paddingLeft: "0.5rem" }}>
-            <p style={{ margin: 0, textAlign: "center" }}>Parameters:</p>
-            <div
-              style={{
-                marginLeft: "0.5rem",
-                display: "flex",
-                flexDirection: "column",
-                rowGap: "0.5rem",
-                justifyItems: "center",
-                alignItems: "center",
-              }}
-            >
-              {fbEvents
-                .find((event) => event.name === eventType)
-                .parameters.map((parameter) => (
-                  <Input
-                    key={parameter.name}
-                    label={parameter.name}
-                    type={parameter.type === "number" ? "number" : "text"}
-                    minWidth="150px"
-                    onChange={handleDataParams}
-                    value={dataParams[parameter.name]}
-                    id={parameter.name}
-                  />
-                ))}
-              <button onClick={sendEvent} style={{ minWidth: "250px" }}>
-                Send Event
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
+      <EventDetailsInput
+        handleDataParams={handleDataParams}
+        handleEventSelect={handleEventSelect}
+        dataParams={dataParams}
+        eventType={eventType}
+        sendEvent={sendEvent}
+      />
     </div>
   );
 }
---
+++ src/components/EventDetailsInput.jsx
@@ -0,0 +1,83 @@
+import { fbEvents } from "../config/fbEvents";
+import Input from "./Input";
+
+const EventDetailsInput = ({
+  handleEventSelect,
+  eventType,
+  handleDataParams,
+  sendEvent,dataParams
+}) => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      flexDirection: "column",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyItems: "center",
+        alignItems: "center",
+        paddingBottom: "0.5rem",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          columnGap: "0.25rem",
+          paddingBottom: "0.25rem",
+        }}
+      >
+        <label htmlFor="eventType">Event Type:</label>
+        <select id="eventType" onChange={handleEventSelect} value={eventType}>
+          <option>Select event type</option>
+          {fbEvents.map((event) => (
+            <option key={event.name}>{event.name}</option>
+          ))}
+        </select>
+      </div>
+      {eventType && (
+        <span style={{ fontStyle: "italic", fontSize: "0.75rem" }}>
+          {fbEvents.find((event) => event.name === eventType).description}
+        </span>
+      )}
+    </div>
+    {eventType && (
+      <div style={{ paddingLeft: "0.5rem" }}>
+        <p style={{ margin: 0, textAlign: "center" }}>Parameters:</p>
+        <div
+          style={{
+            marginLeft: "0.5rem",
+            display: "flex",
+            flexDirection: "column",
+            rowGap: "0.5rem",
+            justifyItems: "center",
+            alignItems: "center",
+          }}
+        >
+          {fbEvents
+            .find((event) => event.name === eventType)
+            .parameters.map((parameter) => (
+              <Input
+                key={parameter.name}
+                label={parameter.name}
+                type={parameter.type === "number" ? "number" : "text"}
+                minWidth="150px"
+                onChange={handleDataParams}
+                value={dataParams[parameter.name]}
+                id={parameter.name}
+              />
+            ))}
+          <button onClick={sendEvent} style={{ minWidth: "250px" }}>
+            Send Event
+          </button>
+        </div>
+      </div>
+    )}
+  </div>
+);
+
+export default EventDetailsInput
