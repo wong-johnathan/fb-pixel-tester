4d5cc484b0e7f743b58aabe76e510dc76d3a70e9
Glitch (gossamer-outgoing-advantage) <none>
2025-04-03 10:11:08 +0000

    📌🚀 Checkpoint ./src/App.jsx:77106476/24576 ./src/components/EventDetailsInput.jsx:77106476/9993 ./package.json:77106476/2997 ./src/components/Input.jsx:77106476/1683

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { ConfigInput } from "./components/ConfigInput";
 import ReactPixel from "react-facebook-pixel";
 import EventDetailsInput from "./components/EventDetailsInput";
 import UserDetailsInput from "./components/UserDetailsInput";
+import {
+  CustomData,
+  EventRequest,
+  ServerEvent,
+  UserData,
+} from "facebook-nodejs-business-sdk";
 
 function App() {
   const { state, updateState } = useContext(MetaContext);
@@ -14,10 +20,6 @@ function App() {
   const [eventType, setEventType] = useState("ViewContent");
   const [dataParams, setDataParams] = useState({});
   const [userInfo, setUserInfo] = useState({});
-
-  const handleEventSelect = (e) => {
-    setEventType(e.target.value);
-  };
 
   const prepareParamsData = (data) => {
     Object.keys(data).forEach((key) => {
@@ -53,6 +55,48 @@ function App() {
     setDataParams({});
   };
 
+  const sendCAPI = () => {
+    const userData = new UserData();
+    if (userInfo.en) userData.setEmail(userInfo.en);
+    if (userInfo.fn) userData.setFirstName(userInfo.fn);
+    if (userInfo.ln) userData.setLastName(userInfo.ln);
+    if (userInfo.ph) userData.setPhone(userInfo.ph);
+
+    const customData = new CustomData();
+    if (dataParams.content_ids)
+      customData.setContentIds(dataParams.content_ids.split(","));
+    if (dataParams.currency) customData.setCurrency(dataParams.currency);
+    if (dataParams.quantity) customData.setNumItems(dataParams.quantity);
+    if (dataParams.value) customData.setValue(dataParams.value);
+    if (dataParams.content_type)
+      customData.setContentType(dataParams.content_type);
+
+    const serverEvent = new ServerEvent();
+    serverEvent.setEventName(eventType);
+    serverEvent.setEventTime(Math.floor(new Date() / 1000));
+    serverEvent.setEventSourceUrl(window.location.href);
+    serverEvent.setActionSource("website");
+    serverEvent.setUserData(userData);
+    serverEvent.setCustomData(customData);
+
+    const eventRequest = new EventRequest(state.accessToken, state.pixelId);
+    eventRequest.setEvents([serverEvent]);
+    if(state.testEventCode) eventRequest.setTestEventCode(state.testEventCode)
+    eventRequest.execute().then(
+      (response) => {
+        console.log("Response: ", response);
+      },
+      (err) => {
+        console.error("Error: ", err);
+      }
+    );
+    setDataParams({});
+  };
+
+  const handleEventSelect = (e) => {
+    setEventType(e.target.value);
+  };
+
   const handleDataParams = (e) => {
     setDataParams((prevState) => ({
       ...prevState,
@@ -79,7 +123,11 @@ function App() {
     >
       <ConfigInput state={state} handleUpdate={handleUpdate} />
       <hr />
-      <UserDetailsInput onChange={handleUserInfo} userInfo={userInfo} setUserInfo={setUserInfo}/>
+      <UserDetailsInput
+        onChange={handleUserInfo}
+        userInfo={userInfo}
+        setUserInfo={setUserInfo}
+      />
       <hr />
       <EventDetailsInput
         handleDataParams={handleDataParams}
@@ -87,6 +135,7 @@ function App() {
         dataParams={dataParams}
         eventType={eventType}
         sendEvent={sendEvent}
+        sendCAPI={sendCAPI}
       />
     </div>
   );
