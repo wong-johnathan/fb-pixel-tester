e3078b6a4ffa209ee69f1e05d4c168679b9e3ec2
Glitch (gossamer-outgoing-advantage) <none>
2025-04-08 02:42:58 +0000

    ❄️✌️ Checkpoint ./src/main.jsx:77106476/3216

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { MetaProvider } from "./context/PixelContext.jsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <MetaProvider>
-        <App />
-      </MetaProvider>
-    </BrowserRouter>
+    <MetaProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/:id" element={<App />} />
+          <Route index element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </MetaProvider>
   </StrictMode>
 );
