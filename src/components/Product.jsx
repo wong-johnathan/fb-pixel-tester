c39cb7d24efebf337d9320dcf48a87885f593d70
Glitch (gossamer-outgoing-advantage) <none>
2025-04-15 04:01:43 +0000

    🏩🤗 Checkpoint ./src/components/Product.jsx:77106476/16683

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,56 @@
 import { useContext, useState } from "react";
 import { MetaContext } from "../context/PixelContext";
 import { useParams, NavLink } from "react-router";
-import UseDetailsInput from './UserDetailsInput'
+import UserDetailsInput from "./UserDetailsInput";
+import { sendPixel } from "../utils";
+import { faker } from "@faker-js/faker";
 const Product = () => {
   const { state } = useContext(MetaContext);
-  
+
   const [userInfo, setUserInfo] = useState({});
   const { id } = useParams();
   const content = state.catalogContent.find((catalog) => catalog.id === id);
-  
+  const handleUserInfo = (e) => {
+    setUserInfo((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const handleAddToCart = () => {
+    const eventID = faker.string.uuid();
+    const [price, currency] = content.price.split(" ");
+    const dataParams = {
+      content_ids: [content.id],
+      quantity: 1,
+      currency,
+      value: price,
+    };
+    sendPixel({
+      dataParams,
+      eventType: "AddToCart",
+      eventID,
+      pixelId: state.pixelId,
+      userInfo,
+    });
+  };
+
+  const handlePurchase = () => {
+    const eventID = faker.string.uuid();
+    const [price, currency] = content.price.split(" ");
+    const dataParams = {
+      content_ids: [content.id],
+      quantity: 1,
+      currency,
+      value: price,
+    };
+    sendPixel({
+      dataParams,
+      eventType: "Purchase",
+      eventID,
+      pixelId: state.pixelId,
+      userInfo,
+    });
+  };
 
   if (!content) return "No catalog items found";
   return (
@@ -20,6 +62,9 @@ const Product = () => {
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
+        maxWidth: "800px",
+        marginLeft: "auto",
+        marginRight: "auto",
       }}
     >
       <UserDetailsInput
@@ -33,8 +78,8 @@ const Product = () => {
       <p>{content.price}</p>
       <div style={{ display: "flex", columnGap: "0.25rem" }}>
         <NavLink to={window.location.origin}>Back</NavLink>
-        <button>Add to cart</button>
-        <button>Purchase</button>
+        <button onClick={handleAddToCart}>Add to cart</button>
+        <button onClick={handlePurchase}>Purchase</button>
       </div>
     </div>
   );
